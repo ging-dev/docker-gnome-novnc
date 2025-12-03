456edd0969fd34e43822e7150b0b812af2dafddb
ging-dev <thanh1101dev@gmail.com>
2025-12-03 02:55:43 +0000

    feat: use dump-init

--- start.sh
+++ start.sh
@@ -1,12 +1,4 @@
-#!/bin/bash
-
-shutdown() {
-    sudo service dbus stop
-    tigervncserver -kill :1
-    exit 0
-}
-
-trap shutdown SIGTERM SIGINT SIGQUIT
+#!/usr/bin/dumb-init /bin/sh
 
 VNC_PASSWORD=${VNC_PASSWORD:-123456}
 GEOMETRY=${GEOMETRY:-1024x768}
@@ -14,6 +6,5 @@ GEOMETRY=${GEOMETRY:-1024x768}
 echo $VNC_PASSWORD | vncpasswd -f > .config/tigervnc/passwd
 
 sudo service dbus start
-tigervncserver -geometry $GEOMETRY
-/usr/share/novnc/utils/novnc_proxy --vnc localhost:5901 > /dev/null 2>&1 &
-wait $!
+tigervncserver :1 -geometry $GEOMETRY -fg &
+/usr/share/novnc/utils/novnc_proxy --vnc localhost:5901
